59debcfd2d455a1c31479a546d89fcf6e1cfcd97
Manishopn <manishopnkumar@gmail.com>
2024-07-27 18:21:54 +0530

    commit

--- src/components/Question.js
+++ src/components/Question.js
@@ -2,12 +2,12 @@ import React from 'react'
 import Option from './Option'
 
 export default function Question({question, dispatch, answer}) {
-  //
+  
   console.log(question)
   return (     
     <>    
       <h3>{question.question}</h3>   
-      <Option question={question} dispatch= {dispatch}  answer= {answer}/>
+      <Option question={question} dispatch= {dispatch} answer= {answer}/>
       
       </>  
        
